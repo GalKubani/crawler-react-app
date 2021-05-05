ad5f1968b44dc55e4f16d7d58f2b54748d982e41
gal kubani <galkubani@gmail.com>
2021-05-05 21:52:09 +0300

    added option to begin scrape request from UI

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import Axios from 'axios'
 
-const Home=()=>(
-    <div className="home">
-        <div>Home</div>
+
+
+const scrapeURL=`http://localhost:4040/start-scarping`
+
+const Home=()=>{
+    const onSubmit=(e)=>{
+        e.preventDefault()
+        let url=e.target.children[1].value
+        let maxDepth=e.target.children[2].value
+        let maxPages=e.target.children[3].value
+        Axios.post(scrapeURL,{
+            url,
+            maxDepth,
+            maxPages,
+            messageBody:url,
+            QueueName:url
+        }).then((res)=>{
+            console.log(res)
+        })
+    }
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <h3>Please enter these paramaters in order to scrape</h3>
+            <input type="text" placeholder="URL" />
+            <input type="number" placeholder="Max depth" />
+            <input type="number" placeholder="Max total pages" />
+
+            <button type="submit">Submit</button>
+        </form>
     </div>
 )
+}
 
 export default Home
